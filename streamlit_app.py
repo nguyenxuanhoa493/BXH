bf22d4ff0e0ee4e358754e33e9e2de3a749068cf
nguyenxuanhoa493 <65268672+nguyenxuanhoa493@users.noreply.github.com>
2023-06-05 16:44:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -142,7 +142,8 @@ def show_rank(rank):
     st.dataframe(rank,use_container_width=True, hide_index=True)
 
 def get_rank(loc, round1, round2):
-    rank1, rank2 = False
+    rank1 = False
+    rank2 = False
     if round1:
         rank1= bvl.rank(round1)
     if round2:
