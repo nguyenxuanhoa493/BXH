6d02917a0e3b5da40f583baaad9fab80de487f4e
nguyenxuanhoa493 <65268672+nguyenxuanhoa493@users.noreply.github.com>
2023-06-05 17:47:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ def merge_rank(rank1, rank2):
     rank = [i for i in rank.values()]
     rank = pd.DataFrame(rank)
     rank.sort_values(by=['total_score', 'total_spent_time'], inplace=True,ascending = [False, True])
-    rank['spent_time'] = pd.to_datetime(rank['spent_time'], format='%M:%S', errors='coerce')  
+    rank['spent_time'] = pd.to_datetime(rank["spent_time"], unit='s').dt.strftime("%H:%M:%S")
     
 
     rank = rank[['code', 'name', 'org', 'score', 'spent_time', 'score2', 'spent_time2', 'total_score','total_spent_time']]
